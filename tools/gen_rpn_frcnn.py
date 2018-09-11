937c512a65e1e8383d76d5ae94c1adfdbcd78498
Aniket Adnaik <aniket.adnaik@gmail.com>
2018-09-11 15:43:14 -0700

    Region proposal generation scripts

---
+++ tools/gen_rpn_frcnn.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python2
+
+# Copyright (c) 2017-present, Facebook, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+##############################################################################
+
+"""This scripts generate region proposal file using fastre rcnn and results 
+are stroed in a pickle file.
+Expects environment variable CAFFE2_HOME to be set to valid caffe2 distrbution
+Requires setting '__C.MODEL.FASTER_RCNN = True' in detectron/core/config.py in order
+make use of faster rcnn. Configuration is set programitically in the code below.
+"""
+
+"""Perform inference on a single image or all images with a certain extension
+(e.g., .jpg) in a folder.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from collections import defaultdict
+import argparse
+import cv2  # NOQA (Must import before importing caffe2 due to bug in cv2)
+import glob
+import logging
+import os
+import sys
+import time
+import pickle
+
+
+sys.path.append(os.environ['CAFFE2_HOME'])
+from caffe2.python import workspace
+
+from detectron.core.config import assert_and_infer_cfg
+from detectron.core.config import cfg
+from detectron.core.config import merge_cfg_from_file
+from detectron.utils.io import cache_url
+from detectron.utils.logging import setup_logging
+from detectron.utils.timer import Timer
+import detectron.core.test_engine as infer_engine
+import detectron.datasets.dummy_datasets as dummy_datasets
+import detectron.utils.c2 as c2_utils
+import detectron.utils.vis as vis_utils
+import pdb
+
+c2_utils.import_detectron_ops()
+
+# OpenCL may be enabled by default in OpenCV3; disable it because it's not
+# thread safe and causes unwanted GPU memory allocations.
+cv2.ocl.setUseOpenCL(False)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='End-to-end inference')
+    parser.add_argument(
+        '--cfg',
+        dest='cfg',
+        help='cfg model file (/path/to/model_config.yaml)',
+#        default=None,
+        default='configs/12_2017_baselines/e2e_faster_rcnn_R-101-FPN_2x.yaml',
+        type=str
+    )
+    parser.add_argument(
+        '--wts',
+        dest='weights',
+        help='weights model file (/path/to/model_weights.pkl)',
+#        default=None,
+        default='https://s3-us-west-2.amazonaws.com/detectron/35857952/12_2017_baselines/e2e_faster_rcnn_R-101-FPN_2x.yaml.01_39_49.JPwJDh92/output/train/coco_2014_train%3Acoco_2014_valminusminival/generalized_rcnn/model_final.pkl',
+        type=str
+    )
+    parser.add_argument(
+        '--output-dir',
+        dest='output_dir',
+        help='directory for visualization pdfs (default: /diskc/temp_files/infer_simple)',
+        default='/diskc/temp_files/predict_output',
+        type=str
+    )
+    parser.add_argument(
+        '--image-ext',
+        dest='image_ext',
+        help='image file name extension (default: jpg)',
+        default='jpg',
+        type=str
+    )
+    parser.add_argument(
+        '--always-out',
+        dest='out_when_no_box',
+        help='output image even when no object is found',
+        action='store_true'
+    )
+    parser.add_argument(
+        '--image_path', 
+        dest='im_or_folder',
+        help='image or folder of images', 
+        default=None
+    )
+    parser.add_argument(
+        '--output-ext',
+        dest='output_ext',
+        help='output image file format (default: pdf)',
+        default='pdf',
+        type=str
+    )
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+    return parser.parse_args()
+
+
+def main(args):
+    logger = logging.getLogger(__name__)
+
+    merge_cfg_from_file(args.cfg)
+    cfg.NUM_GPUS = 1
+    args.weights = cache_url(args.weights, cfg.DOWNLOAD_CACHE)
+    assert_and_infer_cfg(cache_urls=False)
+    cfg.MODEL.FASTER_RCNN = True
+
+    assert not cfg.MODEL.RPN_ONLY, \
+        'RPN models are not supported'
+    assert not cfg.TEST.PRECOMPUTED_PROPOSALS, \
+        'Models that require precomputed proposals are not supported'
+
+    model = infer_engine.initialize_model_from_cfg(args.weights)
+    dummy_coco_dataset = dummy_datasets.get_coco_dataset()
+
+    if os.path.isdir(args.im_or_folder):
+        im_list = glob.iglob(args.im_or_folder + '/*.' + args.image_ext)
+    else:
+        im_list = [args.im_or_folder]
+    logger.info('Processing -> {}'.format(im_list[0]))
+
+    for i, im_name in enumerate(im_list):
+        im_basename = os.path.basename(im_name)
+        out_name = os.path.join(
+            args.output_dir, '{}'.format(im_basename + '.' + args.output_ext)
+        )
+        logger.info('Processing {} -> {}'.format(im_name, out_name))
+        im = cv2.imread(im_name)
+        timers = defaultdict(Timer)
+        t = time.time()
+        with c2_utils.NamedCudaScope(0):
+            cls_boxes, cls_segms, cls_keyps = infer_engine.im_detect_all(
+                model, im, None, timers=timers
+            )
+
+            result_file = os.path.join(args.output_dir, im_basename + '_bbox.pkl')
+            with open(result_file, 'wb') as f:
+                boxes, _, _, _= vis_utils.convert_from_cls_format(cls_boxes, cls_segms, cls_keyps)
+                print(boxes.shape)
+                pickle.dump(cls_boxes, f)
+
+        logger.info('Inference time: {:.3f}s'.format(time.time() - t))
+        for k, v in timers.items():
+            logger.info(' | {}: {:.3f}s'.format(k, v.average_time))
+        if i == 0:
+            logger.info(
+                ' \ Note: inference on the first image will be slower than the '
+                'rest (caches and auto-tuning need to warm up)'
+            )
+        # Visualization of bounding boxes
+#        vis_utils.vis_one_image(
+#            im[:, :, ::-1],  # BGR -> RGB for visualization
+#            im_name,
+#            args.output_dir,
+#            cls_boxes,
+#            cls_segms,
+#            cls_keyps,
+#            dataset=dummy_coco_dataset,
+#            box_alpha=0.3,
+#            show_class=True,
+#            thresh=0.1,
+#            kp_thresh=2,
+#            ext=args.output_ext,
+#            out_when_no_box=args.out_when_no_box
+#         )
+
+
+
+if __name__ == '__main__':
+    workspace.GlobalInit(['caffe2', '--caffe2_log_level=0'])
+    setup_logging(__name__)
+    args = parse_args()
+    main(args)
